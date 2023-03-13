bc85d97ce481e6e14e233f2e047cb12ee1eccc59
kmmkyung <01aldrn10@gmail.com>
2023-03-13 10:38:58 +0900

    f

--- 00.팀프로젝트/media_js.js
+++ 00.팀프로젝트/media_js.js
@@ -1,13 +1,14 @@
 window.addEventListener("DOMContentLoaded",linkFn);
 
 function linkFn(){
-  console.log("링크 로딩완료!");
+  // console.log("링크 로딩완료!");
 
-  //. 로고 대상 선정
-  const logo = document.querySelector(".logo a");
-
-    logo.onclick = (e) => {
+  // 로고 대상 선정
+  const logo = document.querySelectorAll(".logo a");
+  for(let x of logo){
+    x.onclick = (e) => {
         e.preventDefault();
         location.href = "main_page.html";
     }; //////// click ///////////
+  }
 } ////////// linkFn함수 //////////////

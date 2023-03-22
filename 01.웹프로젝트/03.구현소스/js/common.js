2147c36ff75f8223385b310801215d5d87dc4c2e
kmmkyung <01aldrn10@gmail.com>
2023-03-22 13:05:10 +0900

    dd

--- 01.웹프로젝트/03.구현소스/js/common.js
+++ 01.웹프로젝트/03.구현소스/js/common.js
@@ -8,6 +8,25 @@ window.addEventListener("DOMContentLoaded",()=>{
 
     cg("로딩완료!");
 
+    /*-------------------------
+     page2 - new menu 슬라이드 
+     -------------------------*/
+    const p2_Slide = document.querySelector(".slide ul");
+        let num = 0;
+        document.querySelector(".rbtn").onclick=()=>{
+            console.log("P2_오른쪽 버튼")
+            p2_Slide.style.left = (num++*-350) + "px";
+            p2_Slide.style.transition = ".4s";
+        }
+        document.querySelector(".lbtn").onclick=()=>{
+            console.log("P2_왼쪽 버튼")
+            p2_Slide.style.left = (num--*-350) + "px";
+            p2_Slide.style.transition = ".4s";
+        }
+
+    /*-------------------------
+     footer - svg 로고 삽입 
+     -------------------------*/
     // svg 대상 : .page_6-logo
     const logoSVG = qs(".page_6-logo");
 
---
+++ 01.웹프로젝트/03.구현소스/js/menudata.js
@@ -0,0 +1,5 @@
+// 메뉴 관련 데이터 JS - menudata.js ///////
+
+const menudt = {
+    ""
+}

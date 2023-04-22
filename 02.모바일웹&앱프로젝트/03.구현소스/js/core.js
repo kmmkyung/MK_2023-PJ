5f59bc3b0c3a10fc8e061b0328bcaf8ef039c270
MK <01aldrn10@gmail.com>
2023-04-23 02:29:18 +0900

    2

--- 02.모바일웹&앱프로젝트/03.구현소스/js/core.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/core.js
@@ -2,8 +2,8 @@
 window.addEventListener("DOMContentLoaded",()=>{
   console.log("core.js 로딩완료")
 
-  const logoSVG1 = document.querySelector(".nav_Logo1 div");
-  const logoSVG2 = document.querySelector(".nav_Logo2 div");
+  const logoSVG1 = document.querySelector(".nav_Logo1");
+  const logoSVG2 = document.querySelector(".nav_Logo2");
   // console.log(logoSVG1,logoSVG2)
 
   logoSVG1.innerHTML = svgData.logo_1;

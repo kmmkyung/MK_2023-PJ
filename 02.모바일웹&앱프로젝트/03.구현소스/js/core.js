a8fd6b61879ad8148c4b156a9bbaf9442b99e439
MK <01aldrn10@gmail.com>
2023-04-27 20:36:52 +0900

    s

--- 02.모바일웹&앱프로젝트/03.구현소스/js/core.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/core.js
@@ -2,12 +2,37 @@
 window.addEventListener("DOMContentLoaded",()=>{
   console.log("core.js 로딩완료")
 
+  /********************
+    gnb에 로고 넣기
+  ********************/
   const logoSVG1 = document.querySelector(".nav_Logo1");
   const logoSVG2 = document.querySelector(".nav_Logo2");
   // console.log(logoSVG1,logoSVG2)
 
   logoSVG1.innerHTML = svgData.logo_1;
   logoSVG2.innerHTML = svgData.logo_2;
+
+
+  /********************
+    gnb 메뉴 넣기
+  ********************/
+  // 1. 대상 선정
+  const nav = document.querySelector(".nav");
+  console.log(nav);
+
+  // 2. 변수
+  let hcode ="";
+
+  // 3. 구조화
+  hcode += 
+  for(let sm in gnbdata){
+    hcode += `
+
+    `;
+  }
+
+      // 4. GNB 박스에 출력하기
+      nav.innerHTML = hcode;
 
 }) ///////////////////////// load /////////////////////////
 

10b1604229dbb5961f3e4154dac453380a567e31
MK <01aldrn10@gmail.com>
2023-04-27 20:02:50 +0900

    dd

--- 02.모바일웹&앱프로젝트/03.구현소스/js/gnbdata.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/gnbdata.js
@@ -10,11 +10,20 @@ const gnbdata = {
     },
     "Men":{
         "Ready-to-wear":["Coats & Jackets","Knitwear","Jeans & Trousers","Shirts","Tops","Sweatshirts"],
+        "Shoes":["Replica","Tabi","Reebok","Sneakers","Boots & Ankle Boots","Flats"],
+        "Bags":["5AC","Glam Slam","Snatched","New Lock"],
+        "Accessories":["Eyewear","Small Leather Goods","Jewellery","Other Accessories"],
+        "Fragrances":["Replica","(untitled)","All Candles","All Fragrances","Mutiny"]
     },
     "All Gender":{
-
+        "Ready-to-wear":["Knitwear","Coats & Jackets","Jeans & Trousers","Skirts","Tops & Bodysuits","Dresses","Shirts","Sweatshirts"],
+        "Shoes":["Replica","Tabi","Reebok","Sneakers","Flats","Boots & Ankle Boots","Heels"],
+        "Bags":["5AC","Glam Slam","Snatched","New Lock","Shoulder Bags","Handbage"],
+        "Accessories":["Eyewear","Small Leather Goods","Jewellery","Other Accessories"],
+        "Fragrances":["Replica","(untitled)","All Fragrances","All Candles","Mutiny"]
     },
     "Shows":{
-
+        "Co-Ed":["Co-ed 2023","Artisanal Co-Ed SS22","Artisanal Co-Ed SS21"],
+        "Artisanal":["Artisanal AW22","Artisanal AW21"]
     }
 }

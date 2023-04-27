0b6aee181314789576d924fc2cd65ea9c613c9e9
kmmkyung <01aldrn10@gmail.com>
2023-04-27 17:20:38 +0900

    ff

--- 02.모바일웹&앱프로젝트/03.구현소스/js/gnbdata.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/gnbdata.js
@@ -1,7 +1,20 @@
 // gnb 메뉴 관련 데이터 JS - gnbdata.js
 
 const gnbdata = {
-    "a":{
-        "dhshak":[]
+    "Women":{
+        "Ready-to-wear":["Coats & Jackets","Knitwear","Jeans & Trousers","Shirts","Sweatshirts","Tops & Bodysuits","Dresses","Skirts"],
+        "Shoes":["Tabi","Replica","Reebok","Sneakers","Flats","Boots & Ankle Boots","Heels"],
+        "Bags":["5AC","Glam Slam","Snatched","New Lock","Shoulder Bags"],
+        "Accessories":["Eyewear","Small Leather Goods","Jewellery","Other Accessories"],
+        "Fragrances":["Replica","(untitled)","Mutiny","All Candles","All Fragrances"]
     },
+    "Men":{
+        "Ready-to-wear":["Coats & Jackets","Knitwear","Jeans & Trousers","Shirts","Tops","Sweatshirts"],
+    },
+    "All Gender":{
+
+    },
+    "Shows":{
+
+    }
 }

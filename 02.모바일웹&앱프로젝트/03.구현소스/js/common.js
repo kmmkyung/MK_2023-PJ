fb4a094b8458bb595281495ff01219120fafdbf3
MK <01aldrn10@gmail.com>
2023-04-29 21:35:49 +0900

    ㅇㅇ

--- 02.모바일웹&앱프로젝트/03.구현소스/js/common.js
+++ 02.모바일웹&앱프로젝트/03.구현소스/js/common.js
@@ -14,13 +14,21 @@ const mainTitle = document.querySelector(".page1-con>.text-con")
 // console.log("메인텍스트",mainTitle);
 
 function FullImg(){
-    mainImg.style.transition = "all 3s";
-    mainImg.style.scale="1.8";
-    mainImg.style.borderRadius="0px";
-    mainImg.style.filter="brightness(.7)";
-    mainTitle.classList.remove("-hidden")
+    // 이미지가 커지는동안은 아래로 스크롤 할 수 없게 하고싶어요
+    // event.preventDefault;
+
+    mainImg.classList.add("p1_fullimg");
+    setTimeout(() => {
+        mainTitle.classList.remove("-hidden")
+    }, 2500);
 }
 window.addEventListener("wheel",FullImg);
+
+// 페이지가 넘어가면 셋팅 원래대로!(이미지 작아지고 글씨 없어짐)
+// if(){
+//     mainImg.classList.remove("-p1_fullimg")
+//     mainTitle.classList.add("-hidden")
+// }
 // ______________________________page3_____________________________
 /******************************
     함수명: posFixed

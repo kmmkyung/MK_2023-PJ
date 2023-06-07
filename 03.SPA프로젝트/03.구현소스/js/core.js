9712bdda50e222e24131f0db59b701e6efcb4e8a
kmmkyung <01aldrn10@gmail.com>
2023-06-07 17:20:21 +0900

    s

---
+++ 03.SPA프로젝트/03.구현소스/js/core.js
@@ -0,0 +1,7 @@
+///////////////////////// load /////////////////////////
+window.addEventListener("DOMContentLoaded", () => {
+
+  // 헤더 메뉴바 svg
+  document.querySelector(".nav-logo").innerHTML=SvgData.logo  
+
+})///////////////////////// load /////////////////////////

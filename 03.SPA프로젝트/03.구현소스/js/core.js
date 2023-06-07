64de262d8db42d862e43b3593224056acae6ac03
MK <01aldrn10@gmail.com>
2023-06-07 20:53:49 +0900

    s

--- 03.SPA프로젝트/03.구현소스/js/core.js
+++ 03.SPA프로젝트/03.구현소스/js/core.js
@@ -3,5 +3,4 @@ window.addEventListener("DOMContentLoaded", () => {
 
   // 헤더 메뉴바 svg
   document.querySelector(".nav-logo").innerHTML=SvgData.logo  
-
 })///////////////////////// load /////////////////////////

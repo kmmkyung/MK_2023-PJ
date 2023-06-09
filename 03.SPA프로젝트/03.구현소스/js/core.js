038a6f0e233b3b6a625a8af4b239753267614e97
kmmkyung <01aldrn10@gmail.com>
2023-06-09 17:22:21 +0900

    s

--- 03.SPA프로젝트/03.구현소스/js/core.js
+++ 03.SPA프로젝트/03.구현소스/js/core.js
@@ -2,23 +2,25 @@
 window.addEventListener("DOMContentLoaded", () => {
 
 // 파비콘 함수 + 호출 ________________________________________
-// function favicon(){
-//   const faviconImg = [
-//     './images/favicon-frame-1.png',
-//     './images/favicon-frame-2.png',
-//     './images/favicon-frame-3.png',
-//     './images/favicon-frame-4.png',
-//     './images/favicon-frame-5.png',
-//   ]
-//   let faviconCounter = 0;
+function favicon(){
+  const faviconImg = [
+    './images/favicon-frame-1.png',
+    './images/favicon-frame-2.png',
+    './images/favicon-frame-3.png',
+    './images/favicon-frame-4.png',
+    './images/favicon-frame-5.png',
+  ]
+  let faviconCounter = 0;
   
-//   setInterval(function(){
-//     document.querySelector("head").innerHTML='<link rel="icon" href="'+faviconImg[faviconCounter]+'" type="image/png">'
-//     if(faviconCounter == faviconImg.length-1){faviconCounter=0;}
-//     else{faviconCounter++;}
-//    },2000)
-// }; //// setInterval ////
-// favicon();
+  setInterval(function(){
+    
+    // document.querySelector("head").innerHTML='<link rel="icon" href="'+faviconImg[faviconCounter]+'" type="image/png">'
+    document.querySelector(".icon").setAttribute("href",faviconImg[faviconCounter])
+    if(faviconCounter == faviconImg.length-1){faviconCounter=0;}
+    else{faviconCounter++;}
+   },2000)
+}; //// setInterval ////
+favicon();
 //______________________________________________________________
 
   // 헤더 메뉴바 svg

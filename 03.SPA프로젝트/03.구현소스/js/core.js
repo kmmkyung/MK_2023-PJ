f8ea2bbdc78249bea15350a56066fefc94b29f53
MK <01aldrn10@gmail.com>
2023-06-11 18:57:05 +0900

    a

--- 03.SPA프로젝트/03.구현소스/js/core.js
+++ 03.SPA프로젝트/03.구현소스/js/core.js
@@ -1,34 +1,33 @@
 ///////////////////////// load /////////////////////////
 window.addEventListener("DOMContentLoaded", () => {
 
-// 파비콘 함수 + 호출 ________________________________________
+// <head> 파비콘 함수 + 호출 _____________________________________
 function favicon(){
-  const faviconImg = [
-    './images/favicon-frame-1.png',
-    './images/favicon-frame-2.png',
-    './images/favicon-frame-3.png',
-    './images/favicon-frame-4.png',
-    './images/favicon-frame-5.png',
-  ]
   let faviconCounter = 0;
-  
-  setInterval(function(){
-    
+  const faviconImg = ['./images/favicon-frame-1.png',
+    './images/favicon-frame-2.png','./images/favicon-frame-3.png',
+    './images/favicon-frame-4.png','./images/favicon-frame-5.png',]
+
+  setInterval(function(){  
     // document.querySelector("head").innerHTML='<link rel="icon" href="'+faviconImg[faviconCounter]+'" type="image/png">'
     document.querySelector(".icon").setAttribute("href",faviconImg[faviconCounter])
     if(faviconCounter == faviconImg.length-1){faviconCounter=0;}
     else{faviconCounter++;}
-   },800)
-}; //// setInterval ////
-favicon();
+  },800)
+}; //---- favicon 함수 ----//
+favicon(); //---- 호출
 //______________________________________________________________
 
-  // 헤더 메뉴바 svg
-  document.querySelector(".nav-logo").innerHTML=SvgData.logo;
-  // 고정 svg
-  document.querySelector(".svg__1").innerHTML=SvgData.rounde_hand;
-  document.querySelector(".svg__2").innerHTML=SvgData.rounde_cricle;
-  // page1 svg
-  document.querySelector(".page1-svg").innerHTML=SvgData.page1;
+// SVG _________________________________________________________
+// 헤더 메뉴바 svg
+document.querySelector(".nav-logo").innerHTML=SvgData.logo;
+// 고정 원형 svg
+document.querySelector(".svg__1").innerHTML=SvgData.rounde_hand;
+document.querySelector(".svg__2").innerHTML=SvgData.rounde_cricle;
+// page1 center svg
+document.querySelector(".page1-svg").innerHTML=SvgData.page1;
+//______________________________________________________________
+
+
 
 })///////////////////////// load /////////////////////////

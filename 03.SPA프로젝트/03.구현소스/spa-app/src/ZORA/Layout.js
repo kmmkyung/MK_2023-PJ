d2c022f5b346c8cb589eb9878f161f44d0d7358c
MK <01aldrn10@gmail.com>
2023-06-14 10:36:49 +0900

    s

---
+++ 03.SPA프로젝트/03.구현소스/spa-app/src/ZORA/Layout.js
@@ -0,0 +1,9 @@
+const Layout = () => {
+  return (
+      <>
+
+      </>
+  );
+}; 
+// 내보내기
+export default Layout;
--- 03.SPA프로젝트/03.구현소스/spa-app/src/index.js
+++ 03.SPA프로젝트/03.구현소스/spa-app/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Layout from './ZORA/Layout'
+import Main from './ZORA/Main'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App(){
+  return(
+      // <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter>
+          <Routes>
+             <Route path="/" element={<Layout />}>
+                  <Route index element={<Main />} />
+                  <Route path="main" element={<Main />} />
+             </Route>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+          </Routes>
+      </BrowserRouter>
+  );
+} //////////////// App 컴포넌트 //////////////////
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);

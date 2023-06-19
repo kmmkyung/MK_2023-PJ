e011d51627777d896139d3db241275dd81d008a8
kmmkyung <01aldrn10@gmail.com>
2023-06-19 12:45:08 +0900

    s

--- 03.SPA프로젝트/03.구현소스/spa-app/src/ZORA/modules/ChocoDomeCon.js
+++ 03.SPA프로젝트/03.구현소스/spa-app/src/ZORA/modules/ChocoDomeCon.js
@@ -4,94 +4,131 @@ import "../css/chocodomecon.css";
 // Data
 import ChocoDomeCon_data from "../data/ChocoDomeCon_data";
 // 제이쿼리
-import $ from "jquery"
+import $ from "jquery";
 
-function jqFn(){
-    $(()=>[ 
-        console.log('chocodomecon로딩')
-    ]); //--- JQB ---//
+function jqFn() {
+  $(() => {
+    console.log("chocodomecon로딩");
+    // Chocolate 뽀각 _______________________________________________________
+    const chocolateImg1 = document.querySelectorAll(".chocolatepack")
+    const chocolateImg2 = document.querySelectorAll(".chocolatebreak")
+    // console.log(chocolateImg1);
+    
+    chocolateImg2.forEach((ele,idx)=>{
+      // console.log('ele',ele);
+      // console.log('idx',idx);
+      // 초콜릿에 마우스 오버시! 뽀각
+      ele.addEventListener("mouseenter",function(){
+        this.style.opacity=1;
+        chocolateImg1[idx].style.opacity=0;
+        // console.log('chocolateImg1배열순번!',chocolateImg1[idx]);
+      })
+      ele.addEventListener("mouseout",function(){
+        this.style.opacity=0;
+        chocolateImg1[idx].style.opacity=1;
+      })
+    })
+  
+  }); //--- JQB ---//
 } //_______________ jqFn _______________//
 
-const setcss_wrap=[
-    {
-        position: "relative"
-    },
-    {
-        top:"100vh",
-        position: "absolute",
-    },
-    {
-        top:"200vh",
-        position: "absolute",
-    },
-    {
-        top:"300vh",
-        position: "absolute",
-    },
-    {
-        top:"400vh",
-        position: "absolute",
-    }
-]
+const setcss_wrap = [
+  {
+    position: "relative",
+  },
+  {
+    top: "100vh",
+    position: "absolute",
+  },
+  {
+    top: "200vh",
+    position: "absolute",
+  },
+  {
+    top: "300vh",
+    position: "absolute",
+  },
+  {
+    top: "400vh",
+    position: "absolute",
+  },
+];
 const setcss_bg = [
-    {
-        height: "480vh",
-        backgroundColor:"var(--org)"
-    },
-    {
-        height:"200vh",
-        backgroundColor:"var(--blue)"
-    },
-    {
-        height:"200vh",
-        backgroundColor:"var(--red)"
-    },
-    {
-        height:"180vh",
-        backgroundColor:"var(--yell)"
-    },
-    {
-        height:"100vh",
-        backgroundColor:"var(--pink)"
-    },
+  {
+    height: "480vh",
+    backgroundColor: "var(--org)",
+  },
+  {
+    height: "200vh",
+    backgroundColor: "var(--blue)",
+  },
+  {
+    height: "200vh",
+    backgroundColor: "var(--red)",
+  },
+  {
+    height: "180vh",
+    backgroundColor: "var(--yell)",
+  },
+  {
+    height: "100vh",
+    backgroundColor: "var(--pink)",
+  },
 ];
 // jqFn();
 
-
 // 컴포넌트 출력용
-function ChocoDomeCon(){
-  return(
+function ChocoDomeCon() {
+  return (
     <>
-        <section id="main">
-        {
-            ChocoDomeCon_data.main.map((v,i)=>
-                <div className="ChocoDomeCon__warp" key={i} style={setcss_wrap[i]}>
-                    <div className="ChocoDomeCon__con">
-                        <div className="ChocoDomeCon__img">
-                            <img className="ChocoDomeCon__img-1 chocolatepack" src={v.img1} alt="이미지" />
-                            <img className="ChocoDomeCon__img-2  chocolatebreak" src={v.img2} alt="이미지" />
-                        </div>
-                        <div className="ChocoDomeCon__text">
-                            <h2 className="ChocoDomeCon__text-tit">{v.tit}<br/>{v.tit2}</h2>
-                            <button className="ChocoDomeCon__text-btn btn-B">{v.btn}</button>
-                        </div>
-                    </div>
-                    {
-                        i !== 4 && 
-                        <div className="ChocoDomeCon__back" data-seq={i} style={setcss_bg[i]}></div>                
-                    }
-                    {
-                        i === 4 &&
-                        <div className="chocolate-5__backwrap">
-                            <div className="ChocoDomeCon__back" data-seq={i} style={setcss_bg[i]}></div>
-                        </div>
-                    }
-                </div>
-            )
-        }
-        </section>
-    {/* {jqFn()} */}
+      <section id="main">
+        {ChocoDomeCon_data.main.map((v, i) => (
+          <div className="ChocoDomeCon__warp" key={i} style={setcss_wrap[i]}>
+            <div className="ChocoDomeCon__con">
+              <div className="ChocoDomeCon__img">
+                <img
+                  className="ChocoDomeCon__img-1 chocolatepack"
+                  src={v.img1}
+                  alt="이미지"
+                />
+                <img
+                  className="ChocoDomeCon__img-2  chocolatebreak"
+                  src={v.img2}
+                  alt="이미지"
+                />
+              </div>
+              <div className="ChocoDomeCon__text">
+                <h2 className="ChocoDomeCon__text-tit">
+                  {v.tit}
+                  <br />
+                  {v.tit2}
+                </h2>
+                <button className="ChocoDomeCon__text-btn btn-B">
+                  {v.btn}
+                </button>
+              </div>
+            </div>
+            {i !== 4 && (
+              <div
+                className="ChocoDomeCon__back"
+                data-seq={i}
+                style={setcss_bg[i]}
+              ></div>
+            )}
+            {i === 4 && (
+              <div className="chocolate-5__backwrap">
+                <div
+                  className="ChocoDomeCon__back"
+                  data-seq={i}
+                  style={setcss_bg[i]}
+                ></div>
+              </div>
+            )}
+          </div>
+        ))}
+      </section>
+      {jqFn()}
     </>
-  )
+  );
 } //______________ domCon_______________//
 export default ChocoDomeCon;

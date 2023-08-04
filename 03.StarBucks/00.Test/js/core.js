c1d74faa38bde1168e9a07c61bf281a077fe85de
MK <01aldrn10@gmail.com>
2023-08-04 17:56:26 +0900

    s

--- 03.StarBucks/00.Test/js/core.js
+++ 03.StarBucks/00.Test/js/core.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded',function(){
   const searchIcon = document.querySelector('.search i')
   searchInput.addEventListener('focus',function(){
     searchIcon.classList.add('-opacity')
-    searchInput.setAttribute('placeholder','검색어를 입력해 주세요');
+    searchInput.setAttribute('placeholder',' 검색어를 입력해 주세요');
   })
   searchInput.addEventListener('blur',function(){
     searchIcon.classList.remove('-opacity')

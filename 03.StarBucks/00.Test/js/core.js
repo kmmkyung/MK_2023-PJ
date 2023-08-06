a6746bc0429d45c1a0dcb010f19cd21bb8d83a65
MK <01aldrn10@gmail.com>
2023-08-06 23:27:14 +0900

    s

--- 03.StarBucks/00.Test/js/core.js
+++ 03.StarBucks/00.Test/js/core.js
@@ -1,5 +1,47 @@
 window.addEventListener('DOMContentLoaded',function(){
 
+  // [ 메인메뉴 메뉴리스트 적용 ]
+  const nav = document.querySelector('.main-menu')
+  let mainMenuCode = '';
+  mainMenuCode += `<ul class="menu">`
+  // 메뉴 타이틀 title=COFFEE,MENU,STORE...
+  for(let title in navData){
+    mainMenuCode += `
+      <li class="menu-item">
+        <a href="#">
+          <div class="menu-item__name">${title}</div>
+        </a>
+        <div class="menu-item__contents -hidden">
+        <ul class="contents-menu">
+        `
+    // 서브메뉴타이틀
+    for(let sTitle in navData[title]){
+      mainMenuCode += `
+          <li class="contents-menu__list ">
+            <h4>${sTitle}</h4>
+            <ol>
+      `
+      // 하위메뉴
+        for(let list of navData[title][sTitle]){
+          mainMenuCode += `
+              <li>${list}</li>
+              `
+            }
+            mainMenuCode += `
+            </ol>
+          </li>
+            `
+          }
+          mainMenuCode += `
+        </ul>
+      </div>
+    </li>
+    `
+  }
+  mainMenuCode += `</ul>`
+  nav.innerHTML = mainMenuCode;
+  
+  // 서브메뉴 서치아이콘
   const searchInput = document.querySelector('.search input') 
   const searchIcon = document.querySelector('.search i')
   searchInput.addEventListener('focus',function(){

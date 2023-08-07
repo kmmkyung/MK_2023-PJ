8e5476d4e9dffd47bba74fb7e61bde8d9667066e
MK <01aldrn10@gmail.com>
2023-08-08 00:05:01 +0900

    s

---
+++ 03.STARBUCKS/00.Test/js/main.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded',function(){
+  window.addEventListener('scroll',function(){
+    const badge = document.querySelector('.badge')
+    if(window.scrollY>800){
+      badge.classList.add('-opacity')
+      badge.style.pointerEvents='none';
+    }
+    else{
+      badge.classList.remove('-opacity')
+      badge.style.pointerEvents='all';
+    }
+  })
+})

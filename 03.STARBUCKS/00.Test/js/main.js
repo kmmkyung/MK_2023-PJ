d9877c40ca54072f8720ac45939d6e684493d980
MK <01aldrn10@gmail.com>
2023-08-11 02:28:18 +0900

    s

--- 03.STARBUCKS/00.Test/js/main.js
+++ 03.STARBUCKS/00.Test/js/main.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded',function(){
+
+  // [ 베찌 ]
   window.addEventListener('scroll',function(){
     const badge = document.querySelector('.badge')
     if(window.scrollY>800){
@@ -11,5 +13,13 @@ window.addEventListener('DOMContentLoaded',function(){
     }
   })
 
+  // [ 공지사항 - 공지 세로 슬라이드 ]
+  function noticeSlide(){
+    const noticeUl = document.querySelector('.notice-line__notice ul')
+    const noticeLi = document.querySelectorAll('.notice-line__notice ul li')
+    
+  }
+  // [ 공지사항 - 프로모션 아코디언 ]
+  // [ 공지사항 - 프로모션 가로 슬라이드 ]
   
 })

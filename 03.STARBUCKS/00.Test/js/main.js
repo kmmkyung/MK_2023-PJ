016db6ba3a7a0efd68f9d61bbcd710df20eda031
MK <01aldrn10@gmail.com>
2023-08-12 18:21:52 +0900

    s

--- 03.STARBUCKS/00.Test/js/main.js
+++ 03.STARBUCKS/00.Test/js/main.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded',function(){
   // [ 베찌 ]
   window.addEventListener('scroll',function(){
     const badge = document.querySelector('.badge')
-    if(window.scrollY>800){
+    if(window.scrollY>500){
       badge.classList.add('-opacity')
       badge.style.pointerEvents='none';
     }
@@ -20,6 +20,26 @@ window.addEventListener('DOMContentLoaded',function(){
     
   }
   // [ 공지사항 - 프로모션 아코디언 ]
+  function promotionAccordion(){
+    const promotionAccBtn = document.querySelector('.notice-line__promotion i');
+    const promotioncontent = document.querySelector('#notice-promotionBanner')
+    let promotionBanner = false;
+    promotionAccBtn.addEventListener('click',function(){
+      promotionBanner = !promotionBanner
+      if(promotionBanner){
+        promotionAccBtn.classList.toggle('fa-chevron-up')
+        promotionAccBtn.classList.toggle('fa-chevron-down')
+        promotioncontent.style.height = 658+'px'
+      }
+      else{
+        promotionAccBtn.classList.toggle('fa-chevron-up')
+        promotionAccBtn.classList.toggle('fa-chevron-down')
+        promotioncontent.style.height = 0
+      }
+    })
+  }
+  promotionAccordion()
+
   // [ 공지사항 - 프로모션 가로 슬라이드 ]
   
 })

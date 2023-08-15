7bfee10e6058e2202bd9b047567afee70fc012db
MK <01aldrn10@gmail.com>
2023-08-16 03:07:17 +0900

    s

--- 03.STARBUCKS/00.Test/js/main.js
+++ 03.STARBUCKS/00.Test/js/main.js
@@ -13,12 +13,23 @@ window.addEventListener('DOMContentLoaded',function(){
     }
   })
 
+
   // [ 공지사항 - 공지 세로 슬라이드 ]
   function noticeSlide(){
     const noticeUl = document.querySelector('.notice-line__notice ul');
-    const noticeLi = document.querySelectorAll('.notice-line__notice ul li');
-    let noticeIndex = 0;
+    setInterval(function(){
+      const noticeLi = document.querySelectorAll('.notice-line__notice ul li');
+      noticeUl.insertBefore(noticeLi[noticeLi.length-1],noticeLi[0]);
+      noticeUl.style.height = -100 +'%';
+      noticeUl.style.transition = "none";
+      setTimeout(function(){
+        noticeUl.style.height = '0';
+        noticeUl.style.transition = "height .4s ease-in-out";
+      },1)
+    },4000)
   }
+  noticeSlide()
+
 
   // [ 공지사항 - 프로모션 아코디언 ]
   function promotionAccordion(){
@@ -31,37 +42,46 @@ window.addEventListener('DOMContentLoaded',function(){
         promotionAccBtn.classList.toggle('fa-chevron-up');
         promotionAccBtn.classList.toggle('fa-chevron-down');
         promotioncontent.style.height = 658+'px';
+        clearAuto()
       }
       else{
         promotionAccBtn.classList.toggle('fa-chevron-up');
         promotionAccBtn.classList.toggle('fa-chevron-down');
         promotioncontent.style.height = 0;
+        clearInterval(autoSlideInterval);
+        clearTimeout(autoSlideTimeout);
       }
     })
   }
   promotionAccordion()
   
-  
+
   // [ 공지사항 - 프로모션 가로 슬라이드 ]
   const arrowBtn = document.querySelectorAll('.promotionBanner-arrow i');
   const slideUl = document.querySelector('.promotionBanner-box');
-  const slideLl = document.querySelectorAll('.promotionBanner');
-  const bullet = document.querySelectorAll('.fa-circle');
+  const pause = document.querySelector('.fa-pause');
+  const play = document.querySelector('.fa-play');
   let clickClick = 0;
+  let autoSlideInterval
+  let autoSlideTimeout
   
   // 버튼 이벤트 설정 및 분기(L-0 / R-1)
   arrowBtn.forEach(function(v,i){
     v.addEventListener('click',function(){
+      clearAuto()
       promotionSlide(i)
-      promotionchange(bullet)
-      promotionchange(slideLl)
+      promotionOn()
+      if(play.classList!=='-hidden'){
+        play.classList.add('-hidden');
+        pause.classList.remove('-hidden')
+      }
     })
   })
 
   // 슬라이드 이동 함수
   function promotionSlide(i){
-    // li 수집
     const slideLl = document.querySelectorAll('.promotionBanner');
+    
     // 광클금지
     if(clickClick) return;
     clickClick = 1;
@@ -88,31 +108,30 @@ window.addEventListener('DOMContentLoaded',function(){
       },1)
     }
   }
+  
+  // 프로모션 가로 슬라이드 On 함수
+  function promotionOn(){
+    const slideLl = document.querySelectorAll('.promotionBanner');
+    const bullet = document.querySelectorAll('.fa-circle');
 
-  // 프로모션 가로 슬라이드 슬라이드 opacity / 블릿 색상 함수
-  function promotionchange(obj){
-    obj.forEach((v,i)=>{
-      let objsum = obj[i].getAttribute('data-seq'); // 0,1,2
-      v.classList.remove('on');
-    })
-    // 오른쪽 버튼을 누르면 2->0->1 순으로 클래스 on 주기
-    // 왼쪽 버튼을 누르면 0->2->1 순으로 클래스 on 주기
+    let nowseq = slideLl[2].getAttribute('data-seq');
+    console.log('현재순번',nowseq);
+    for(let x of slideLl){x.classList.remove('on')};
+    slideLl[nowseq].classList.add('on');
+    for(let x of bullet){x.classList.remove('on')};
+    bullet[nowseq].classList.add('on');
   }
 
   // 프로모션 가로 슬라이드 자동 이동
   function autoSlide(){
     autoSlideInterval = setInterval(function(){
       promotionSlide(1)
-      promotionchange(1)
+      promotionOn()
     },4000)
-    return autoSlideInterval;
   }
-  // autoSlide()
-
-  // 프로모션 가로 슬라이드 멈춤
-  function autoSlideStop(){
-    const pause = document.querySelector('.fa-pause');
-    const play = document.querySelector('.fa-play');
+  
+  // 프로모션 가로 슬라이드 멈춤버튼
+  function pauseBtn(){
     pause.addEventListener('click',()=>{
       clearInterval(autoSlideInterval);
       pause.classList.add('-hidden');
@@ -124,5 +143,19 @@ window.addEventListener('DOMContentLoaded',function(){
       pause.classList.remove('-hidden')
     })
   }
-  autoSlideStop()
+  pauseBtn()
+  
+  // 프로모션 가로 슬라이드 자동 초기화
+  function clearAuto(){
+    clearInterval(autoSlideInterval);
+    clearTimeout(autoSlideTimeout);
+    autoSlideTimeout = setTimeout(autoSlide,4500)
+  }
+
+
+
+
+
+
+  
 })

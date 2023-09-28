65bb6ed11ad573781f160258efe7dc62eec9692a
MK <01aldrn10@gmail.com>
2023-09-28 16:19:48 +0900

    s

--- 00.PORTFOILO/00.Code/js/common.js
+++ 00.PORTFOILO/00.Code/js/common.js
@@ -6,8 +6,24 @@ console.log('common 로딩');
 const headerLogo = $('.logo_W');
 headerLogo.html(svgLogo.logo_w);
 
+// [ header - contact]
+const footerBtn = $('.footer-btn');
+footerBtn.on('click',function(){
+  $(window).scrollTop(document.body.scrollHeight);
+})
 
+// [ footer - z-index]
+const footer = $('footer');
+let mainH = $('#sec-main').height();
 
+$(window).on('scroll',function(){
+  if($(window).scrollTop()>mainH){
+    footer.css({zIndex:'1'})
+  }
+  else{
+    footer.css({zIndex:'-1'})
+  }
+})
 
 
 

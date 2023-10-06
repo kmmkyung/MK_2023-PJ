d09aa199f6b08ea0e55a9f4dc273766d2b640e9e
MK <01aldrn10@gmail.com>
2023-10-06 15:16:38 +0900

    s

--- 00.PORTFOLIO/00.Code/js/main.js
+++ 00.PORTFOLIO/00.Code/js/main.js
@@ -1,19 +1,42 @@
 window.addEventListener('DOMContentLoaded',function(){
 
 // [ 메인페이지 마지막 프로젝트 ]
-const tl = gsap.timeline();
+let tl = gsap.timeline();
 tl.from($('.main-project__imgBox'),{
   opacity:0,
   duration:3
 }).from([$('.main-project h3'),$('.main-project h2'),$('.main-project p')],{
   opacity:0,
   duration:2
-},'-=1.5')
+},'-=1.5');
 
 // [프로젝트 페이지 가로 스크롤]
-const project = gsap.utils.toArry('.project');
-const hiddenPj = gsap.utils.toArry('.hiddenPj');
+const projectPage = document.querySelector('#sec-works');
+const project = gsap.utils.toArray('.project');
+const hiddenPj = gsap.utils.toArray('.hiddenPj');
 const firstPj = document.querySelector('.firstPj');
+
+let secWork = gsap.to(projectPage,{
+  // defaults: { ease: 'none', duration: 1 },
+  ease: 'none',
+  duration: 1,
+  scrollTrigger:{
+    trigger: projectPage,
+    pin:true,
+    scrub:1,
+    end: () => projectPage.scrollWidth - document.documentElement.clientWidth,
+  }
+});
+
+
+let projectTl = gsap.timeline({})
+projectTl.to(project,{
+  scrollTrigger:{
+    containerAnimation: secWork
+  }
+})
+
+
 
 
 })// DOMContentLoadeds

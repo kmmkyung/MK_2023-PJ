2009b0776ca95b4b50cc0919d2e700d5fcc5e145
MK <01aldrn10@gmail.com>
2023-10-07 12:26:05 +0900

    s

--- 00.PORTFOLIO/00.Code/js/common.js
+++ 00.PORTFOLIO/00.Code/js/common.js
@@ -3,11 +3,11 @@ window.addEventListener('DOMContentLoaded',function(){
 // [ header - logo ]
 // const headerLogo = $('.logo_W');
 // headerLogo.html(svgLogo.logo_w);
-// let pageNum = 0 // 전체페이지수
-// function updataPage(){
+let pageNum= 0 // 전체 페이지 변수
+let pageAll = 0 // 전체페이지수
+function updataPage(){
 
-// }
-
+}
 
 // [ header - contact]
 const workBtn = $('.works-btn');

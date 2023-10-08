afa9e8f1e5e34a10e65614ffe392ba3e4e484d42
MK <01aldrn10@gmail.com>
2023-10-08 20:03:00 +0900

    s

--- 00.PORTFOLIO/00.Code/js/sub_01.js
+++ 00.PORTFOLIO/00.Code/js/sub_01.js
@@ -1,13 +1,23 @@
-// [ header - contact]
-const workBtn = $('.main-btn');
-const aboutBtn = $('.site-btn');
-const footerBtn = $('.footer-btn');
-workBtn.on('click',function(){
-  $('html').animate({},300)
-});
-aboutBtn.on('click',function(){
-  $('html').animate({},300)
-});
-footerBtn.on('click',function(){
-  $('html').animate({scrollTop:document.body.scrollHeight},300)
-});
+window.addEventListener('DOMContentLoaded',function(){
+
+  // [ header - contact]
+  const mainBtn = $('.main-btn');
+  const siteBtn = $('.site-btn');
+  const footerBtn = $('.footer-btn');
+
+  mainBtn.on('click',function(){
+    location.href = 'index.html';
+  });
+  siteBtn.on('click',function(){
+    window.open('https://kmmkyung.github.io/PJ-EDIAY/Code/intro.html', '_blank');
+  });
+  footerBtn.on('click',function(){
+    $('html').animate({scrollTop:document.body.scrollHeight},300)
+  });
+
+
+
+
+
+
+})

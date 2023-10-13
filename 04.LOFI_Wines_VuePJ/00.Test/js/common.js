16d8822b9a92e7bce2c52b1ec2333c846e9cd994
MK <01aldrn10@gmail.com>
2023-10-13 18:14:13 +0900

    s

---
+++ 04.LOFI_Wines_VuePJ/00.Test/js/common.js
@@ -0,0 +1,40 @@
+window.addEventListener('DOMContentLoaded',function(){
+  // [로고]
+  const logo = document.querySelector('.logo')
+  logo.innerHTML=logoData
+
+  // [메뉴]
+  const nav = document.querySelector('nav');
+  const hamburger = document.querySelector('.hamburger');
+  const menuItem = document.querySelectorAll('.menu-item');
+  const asideLink = document.querySelector('aside');
+
+  function menuItemFn(ele,time){
+    setTimeout(function(){
+      menuItem[ele].style.transform='translateY(0%)'
+      menuItem[ele].style.opacity='1'
+    },time)
+  }
+  
+  hamburger.addEventListener('click',function(){
+    hamburger.classList.toggle('on');
+    nav.classList.toggle('on');
+    if(nav.classList.contains('on')){
+      menuItemFn(0,400)
+      menuItemFn(1,600)
+      menuItemFn(2,800)
+      setTimeout(()=>{
+        asideLink.style.transform='translateY(0%)'
+        asideLink.style.opacity='1'
+      },400)
+    }
+    else{
+      menuItem.forEach((ele,idx) => {
+        menuItem[idx].style.opacity='0'
+        menuItem[idx].style.transform='translateY(100%)'
+      })
+      asideLink.style.opacity='0'
+      asideLink.style.transform='translateY(100%)'
+    }
+  })
+})

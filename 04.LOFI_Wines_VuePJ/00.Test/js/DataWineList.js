d414be0543913c1f883a3cbdd013500cc97520bd
MK <01aldrn10@gmail.com>
2023-10-20 23:21:28 +0900

    s

---
+++ 04.LOFI_Wines_VuePJ/00.Test/js/DataWineList.js
@@ -0,0 +1,80 @@
+const wineList = [
+  'AUSTRALIA' = [
+    'Ada Wines',
+    'ARC Wines',
+    'Architects of Wine',
+    'Bizzarro',
+    'Das Juice',
+    'Delinquente',
+    'Ephemera',
+    'Good Intentions Wine Co.',
+    'Maison Soleil',
+    'Saep',
+    'Sigurd',
+    'Sonnen'
+  ],
+  'AUSTRIA' = [
+    'Christian Tschida',
+    'Claus Preisinger',
+    'Gut Oggau',
+    'Michael Gindl'
+  ],
+  'FRANCE' = [
+    'Alexandre Jouveaux',
+    'Amaury Beaufort',
+    'Chateau de Beru',
+    'Domaine Bobinet',
+    'Domaine Dame Jeanne',
+    'Domaine David-Beaupère',
+    'Domaine des Fauvettes',
+    'Domaine des Sablonnettes',
+    'Domaine Didon',
+    'Domaine Finot',
+    'Domaine Houillon',
+    'Elodie Jaume',
+    'Francois Dhumes',
+    'Francois St-Lô',
+    'Jérôme Balmet',
+    'Joe Chandellier',
+    'Laurent Saillard',
+    'Le Batossay',
+    'Le Facteur',
+    'Les Errances',
+    'Les Grangeons de l’Albarine',
+    'Les Vignes de Paradis',
+    'Louis Terral',
+    'Lulu Vigneron',
+    'Mai & Kenji Hodgson',
+    'Matassa',
+    'Raphaëlle Guyot',
+    'Thomas Boutin',
+    'Val de Combres'
+  ],
+  'GERMANY'=[
+    'Kühling-Gillot',
+    'Melsheimer'
+  ],
+  'HUNGARY'=[
+    'Peter Wetzer'
+  ],
+  'ITALY' = [
+    'Al di là del Fiume',
+    'Alessandro Viola',
+    'Cantina Margo',
+    'Casè',
+    'Col Tamarie',
+    'Collecapretta',
+    'Colombaia',
+    'Fabio Gea',
+    'Il Roccolo di Monticelli',
+    'Indomiti',
+    'Lammidia',
+    'Pranzegg'
+  ],
+  'NEW ZEALAND' = [
+    'Tincan Wines'
+  ],
+  'UNITED STATES' = [
+    'Hermann J. Wiemer'
+  ]
+]

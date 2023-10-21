cb14afafb8e367e005fe0c0f9951479f1878d298
MK <01aldrn10@gmail.com>
2023-10-21 18:54:45 +0900

    s

---
+++ 04.LOFI_Wines_VuePJ/00.Test/data/DataWineList.js
@@ -0,0 +1,160 @@
+const wineList = [
+  'AUSTRALIA' = [
+    'Ada Wines' =[
+      `Ada is the passion project for James Ellis, who also works full time with Gareth Belton of Gentle Folk in the Adelaide Hills. Ada is a relative newcomer to the South Australian wine scene with their first release being born of the 2018 vintage. A viticulturist at heart James is firmly of the belief that all of his wines are made in the vineyard and he is meticulous with his approach to organics across the sites he manages for Ada and Gareth. Before starting Ada, James had worked his way through many of the nations iconic regions with time spent in vineyard and cellar in the Barossa Valley working for Penfolds, Hunter Valley and McLaren Vale where he met James Erskine of Jauma who introduced him to Gareth.`,
+      `Now James has purchased a house in the Barossa and will continue to work in the hills while sourcing precious small parcels from across South Australia. He crafts wines that are truly expressive of the regions they come from whilst also being produced from the grapes that he loves to drink. Skinsy Eden Riesling, McLaren Vale Grenache, Piccadilly Valley Chardonnay, Basket Range Pinot Noir and Barossa Grenache with more to come as James finds his groove and grows. All Ada grapes are from small growers who farm the land respectively and organically. While winemaking is fairly hands off with the only additions being minimal amounts of SO2 when it is necessary.`,
+      `James’ 2021 release is stellar. All five wines are looking great and will continue to evolve across the year. We are thrilled to be working together and cant wait to watch Ada grow into one of the regions most exciting producers.`,
+      {
+        ID : 00,
+        WINE : 'Cheeky Charlie',  
+        PRODUCER : 'Ada Wines',  
+        COUNTRY : 'Australia',  
+        REGION : 'Adelaide Hills',  
+        VARIETY : 'Riesling',  
+        STYLE : 'White',
+        ABOUT : `100% Riesling from a single dry grown vineyard in Springton, Eden Valley (organic). The fruit was handpicked and de-stemmed to stainless steel. The ferment was hand plunged twice daily and the tank sealed once fermentation was complete. The fruit was allowed to stay on skins for 95 days and then gently basket pressed to neutral french oak barriques. The extended skin contact has enabled every last bit of flavour and texture to be extracted for the beautiful skins. The texture is prolific but not overpowering or detrimental to the balance of the wine. Racked and settled before bottling and contains approx 40ppm total sulphur.`,
+        IMG:'./images/Ada Wines/Cheeky Charlie.jpg'
+      },
+      {
+        ID : 01,
+        WINE : 'The Rift',  
+        PRODUCER : 'Ada Wines',  
+        COUNTRY : 'Australia',  
+        REGION : 'Adelaide Hills',  
+        VARIETY : 'Chardonnay',  
+        STYLE : 'White',
+        ABOUT : `100% Chardonnay comprising 2 clones ( 90% Mendoza clone/ 10% g9v7 sparkling clone) (organic). Handpicked and whole bunch pressed directly to barrel for barrel fermentation. Remains on gross lees without stirring for 7 months. Small amount of sulphur added at bottling (approx 40ppm for this wine).`,
+        IMG:'./images/Ada Wines/The Rift.jpg'
+      },
+      {
+        ID : 02,
+        WINE : 'Clique',  
+        PRODUCER : 'Ada Wines',  
+        COUNTRY : 'Australia',  
+        REGION : 'Adelaide Hills',  
+        VARIETY : 'Shiraz Blend',  
+        STYLE : 'Red',
+        ABOUT : `Clique: Essentially an assemblage of barrels that were chosen for their unique characteristics and inherit early drinkability. Muscat 14% / Shiraz 50% / Grenache 36% all from organic sites across McLaren Vale. The Muscat was fermented on skins for a few days before pressing to french oak. The Grenache and Shiraz components were 100% whole bunch fermented for 14 days before being pressed to a stainless steel tank. James has created an energetic, early drinking fridge red that has both structure and depth of fruit. The red varieties provide the structure and fruit weight whilst the muscat helps elevate everything another octave with aromatics and spice at the forefront.`,
+        IMG:'./images/Ada Wines/Clique.jpg'
+      },
+      {
+        ID : 03,
+        WINE : 'Mad Hattie',  
+        PRODUCER : 'Ada Wines',  
+        COUNTRY : 'Australia',  
+        REGION : 'Adelaide Hills',  
+        VARIETY : 'Grenache Blend',  
+        STYLE : 'Red',
+        ABOUT : `55% Barossa Grenache (70 year old organic vineyard in Cockatoo Valley / 45% McLaren Vale Shiraz (certified BD fruit in McLaren Flat). The Grenache component underwent a short fermented on skins (60% WB) for 5 days before being pressed to neutral french oak barriques. The McLaren Vale Shiraz was hand picked and directly pressed to stainless steel. Once fermenting, the juice was transferred to french oak barriques to finish fermenting. Once sugar and malic dry the wines were racked and blended and transferred back to barrel to rest and develop.`,
+        IMG:'./images/Ada Wines/Mad Hattie.jpg'
+      },
+      {
+        ID : 04,
+        WINE : 'Celui',  
+        PRODUCER : 'Ada Wines',  
+        COUNTRY : 'Australia',  
+        REGION : 'Adelaide Hills',  
+        VARIETY : 'Pinot Noir',  
+        STYLE : 'Red',
+        ABOUT : `100% Pinot Noir from the Monomeith Vineyard comprising of 3 clones ( equal parts 114/115 and 777) (organic). Fruit was handpicked at 3 different stages over 2 weeks to enable each clone optimal ripeness and added to a rolling type ferment comprising 50% WB inclusion. The ferment was hand plunged once daily and then basket pressed to french oak barriques (33% 1 year old). The wine remained on fine lees until bottling when a small amount of sulphur was added (approx 40ppm for this wine).`,
+        IMG:'./images/Ada Wines/Celui.jpg'
+      },
+      {
+        ID : 05,
+        WINE : 'The Nudist',  
+        PRODUCER : 'Ada Wines',  
+        COUNTRY : 'Australia',  
+        REGION : 'Adelaide Hills',  
+        VARIETY : 'Grenache',  
+        STYLE : 'Red',
+        ABOUT : `100% Grenache organically grown, handpicked and partly destemmed (50% whole bunch). The ferment was hand plunged once daily and then basket pressed to french oak barriques after 3 weeks. The wine is a step up in ripeness from past years, it's also the first year with a portion of whole bunch in the ferment and we really like the result. It has a power and roundness on the palate but also has loads of freshness and vibrancy that are driven by its limestone derived structure and minerality. The wine remained on fine lees until bottling when a small amount of sulphur was added (approx 40ppm for this wine)`,
+        IMG:'./images/Ada Wines/The Nudist.jpg'
+      }],
+    'ARC Wines' =
+      {
+        ID : 00,
+        WINE : '',  
+        PRODUCER : 'Ada Wines',  
+        COUNTRY : '',  
+        REGION : '',  
+        VARIETY : '',  
+        STYLE : '',
+        ABOUT : ``,
+        IMG:'./images/ARC Wines/' 
+      },
+    'Architects of Wine',
+    'Bizzarro',
+    'Das Juice',
+    'Delinquente',
+    'Ephemera',
+    'Good Intentions Wine Co.',
+    'Maison Soleil',
+    'Saep',
+    'Sigurd',
+    'Sonnen'
+  ],
+  'AUSTRIA' = [
+    'Christian Tschida',
+    'Claus Preisinger',
+    'Gut Oggau',
+    'Michael Gindl'
+  ],
+  'FRANCE' = [
+    'Alexandre Jouveaux',
+    'Amaury Beaufort',
+    'Chateau de Beru',
+    'Domaine Bobinet',
+    'Domaine Dame Jeanne',
+    'Domaine David-Beaupère',
+    'Domaine des Fauvettes',
+    'Domaine des Sablonnettes',
+    'Domaine Didon',
+    'Domaine Finot',
+    'Domaine Houillon',
+    'Elodie Jaume',
+    'Francois Dhumes',
+    'Francois St-Lô',
+    'Jérôme Balmet',
+    'Joe Chandellier',
+    'Laurent Saillard',
+    'Le Batossay',
+    'Le Facteur',
+    'Les Errances',
+    'Les Grangeons de l’Albarine',
+    'Les Vignes de Paradis',
+    'Louis Terral',
+    'Lulu Vigneron',
+    'Mai & Kenji Hodgson',
+    'Matassa',
+    'Raphaëlle Guyot',
+    'Thomas Boutin',
+    'Val de Combres'
+  ],
+  'GERMANY'=[
+    'Kühling-Gillot',
+    'Melsheimer'
+  ],
+  'HUNGARY'=[
+    'Peter Wetzer'
+  ],
+  'ITALY' = [
+    'Al di là del Fiume',
+    'Alessandro Viola',
+    'Cantina Margo',
+    'Casè',
+    'Col Tamarie',
+    'Collecapretta',
+    'Colombaia',
+    'Fabio Gea',
+    'Il Roccolo di Monticelli',
+    'Indomiti',
+    'Lammidia',
+    'Pranzegg'
+  ],
+  'NEW ZEALAND' = [
+    'Tincan Wines'
+  ],
+  'UNITED STATES' = [
+    'Hermann J. Wiemer'
+  ]
+]

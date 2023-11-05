92c0730e34dcc8bf6045a04405766a2ef72d6b9e
MK <01aldrn10@gmail.com>
2023-11-05 20:02:20 +0900

    s

--- 04.LOFI_Wines_VuePJ/00.Test/js/winesList.js
+++ 04.LOFI_Wines_VuePJ/00.Test/js/winesList.js
@@ -1,14 +1,18 @@
 import DataWineList from "../assets/data/DataWineList.js"
 window.addEventListener('DOMContentLoaded',function(){
 
+  // title
+  const title = document.querySelector('.title>h2');
+  title.textContent = Object.keys(DataWineList.AUSTRALIA)[0]
+
   // wines list 
   // wines list sort
   let winesList = '';
+  let wines = DataWineList.AUSTRALIA['Ada Wines']['LIST'];  
+  wines.sort(function(a,b){
+    return a.WINE<b.WINE ? -1 : 1
+  })
   for(let wines of DataWineList.AUSTRALIA['Ada Wines']['LIST']){
-    const winesName = wines
-    console.log(winesName);
-    
-    
     winesList += `
     <li class="item">
       <h3>${wines.WINE}</h3>
@@ -18,12 +22,34 @@ window.addEventListener('DOMContentLoaded',function(){
   }
   document.querySelector('.item-list').innerHTML = winesList;
 
-
   // wines list eff
+  const itemList = document.querySelector('.item-list');
   const item = document.querySelectorAll('.item');
+  const itemImg = document.querySelector('.wines-imgBox>img')
+  itemImg.setAttribute('src',`./assets/${wines[0].IMG}`)
+
+  if(itemList.scrollHeight >= 360){
+    item[item.length-1].style.marginBottom = '100%'
+  }
   item.forEach(function(ele,idx){
     ele.addEventListener('mouseenter',function(){
-      
+      ele.classList.add('colorB');
+      let notEle = document.querySelectorAll('.item-list>li[class]:not(.colorB)')
+      notEle.forEach(function(ele){
+        ele.classList.add('colorGray');
+      })
+      itemImg.setAttribute('src',`./assets/${wines[idx].IMG}`)
+    })
+    ele.addEventListener('mouseleave',function(){
+      let notEle = document.querySelectorAll('.item-list>li[class]:not(.colorB)')
+      ele.classList.remove('colorB');
+      notEle.forEach(function(ele){
+        ele.classList.remove('colorGray');
+      })
+      itemImg.setAttribute('src',`./assets/${wines[0].IMG}`)
     })
   })
+
+  // Btn
+
 })

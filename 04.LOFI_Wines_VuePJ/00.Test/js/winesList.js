43f005d70d6dbd947d4bf1af3f42d0a43037b306
MK <01aldrn10@gmail.com>
2023-11-06 02:28:27 +0900

    s

--- 04.LOFI_Wines_VuePJ/00.Test/js/winesList.js
+++ 04.LOFI_Wines_VuePJ/00.Test/js/winesList.js
@@ -1,9 +1,6 @@
 import DataWineList from "../assets/data/DataWineList.js"
 window.addEventListener('DOMContentLoaded',function(){
 
-  const itemImg = document.querySelector('.wines-imgBox');
-  itemImg.style.backgroundImage = 'url(./assets/images/Ada Wines/Clique.jpg)';
-
   // title
   const title = document.querySelector('.title>h2');
   title.textContent = Object.keys(DataWineList.AUSTRALIA)[0]
@@ -11,7 +8,9 @@ window.addEventListener('DOMContentLoaded',function(){
   // wines list 
   // wines list sort
   let winesList = '';
-  let wines = DataWineList.AUSTRALIA['Ada Wines']['LIST'];  
+  let wines = DataWineList.AUSTRALIA['Ada Wines']['LIST'];
+
+  
   wines.sort(function(a,b){
     return a.WINE<b.WINE ? -1 : 1
   })
@@ -25,23 +24,27 @@ window.addEventListener('DOMContentLoaded',function(){
   }
   document.querySelector('.item-list').innerHTML = winesList;
   
+  const itemImg = document.querySelector('.wines-imgBox');
+  itemImg.style.backgroundImage = "url('./assets/"+wines[0].IMG+"')";
+  
   // wines list effect
   const itemList = document.querySelector('.item-list');
   const item = document.querySelectorAll('.item');
-
-
+  
+  
   if(itemList.scrollHeight >= 360){
     item[item.length-1].style.marginBottom = '100%'
   }
-
+  
   item.forEach(function(ele,idx){
+    
     ele.addEventListener('mouseenter',function(){
+      itemImg.style.backgroundImage = "url('./assets/"+wines[idx].IMG+"')";
       ele.classList.add('colorB');
       let notEle = document.querySelectorAll('.item-list>li[class]:not(.colorB)')
       notEle.forEach(function(ele){
         ele.classList.add('colorGray');
       })
-      itemImg.style.backgroundImage = `url(../assets/${wines[idx].IMG})`
     })
     ele.addEventListener('mouseleave',function(){
       let notEle = document.querySelectorAll('.item-list>li[class]:not(.colorB)')
@@ -49,7 +52,7 @@ window.addEventListener('DOMContentLoaded',function(){
       notEle.forEach(function(ele){
         ele.classList.remove('colorGray');
       })
-
+      itemImg.style.backgroundImage = "url('./assets/"+wines[0].IMG+"')";
     })
   })
 

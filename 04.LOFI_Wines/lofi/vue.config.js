e6c4f4f8f25939ebff9fc4b09b4e9baebc8edd9e
Min Kyung <01aldrn10@gmail.com>
2023-12-06 08:11:35 +0900

    s

--- 04.LOFI_Wines/lofi/vue.config.js
+++ 04.LOFI_Wines/lofi/vue.config.js
@@ -1,9 +1,8 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-// })
-
-module.exports = {
-	publicPath: process.env.NODE_ENV === 'production'
-    ? '/PJ-LOFI/'
-    : '/'
-}
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({   
+    // module.exports = {
+        publicPath: process.env.NODE_ENV === 'production'
+        ? '/PJ-LOFI/'
+        : '/'
+    // }
+})

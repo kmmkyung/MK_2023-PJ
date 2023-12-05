77d17decdb2431fc0f93fd7efab2d2eaa4763eec
Min Kyung <01aldrn10@gmail.com>
2023-12-05 20:26:01 +0900

    s

--- 04.LOFI_Wines/lofi/vue.config.js
+++ 04.LOFI_Wines/lofi/vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  // transpileDependencies: true
 // publicPath:'/PJ-LOFI'
+publicPath: process.env.NODE_ENV === 'production'
+? '/production-sub-path/'
+: '/'
 })

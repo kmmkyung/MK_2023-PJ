1f97f12593a9ab735a42e8c8d9fb28de21a92e41
Min Kyung <01aldrn10@gmail.com>
2023-12-07 23:10:59 +0900

    s

--- 04.LOFI_Wines/lofi/vue.config.js
+++ 04.LOFI_Wines/lofi/vue.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-	// configureWebpack: {
-    // performance: {
-    // maxEntrypointSize: 10000000,
-    // maxAssetSize: 10000000
-    // }
-	// },
+	configureWebpack: {
+    performance: {
+    maxEntrypointSize: 10000000,
+    maxAssetSize: 10000000
+    }
+	},
 	publicPath: '/PJ-LOFI',
 	// transpileDependencies:true
 }

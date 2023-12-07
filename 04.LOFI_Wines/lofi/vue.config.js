da2be2045bdb7464bc75e89c15c28be7ab7702e2
Min Kyung <01aldrn10@gmail.com>
2023-12-07 17:15:05 +0900

    s

--- 04.LOFI_Wines/lofi/vue.config.js
+++ 04.LOFI_Wines/lofi/vue.config.js
@@ -1,13 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-    transpileDependencies:true,
-	publicPath: process.env.NODE_ENV === 'production'
-    ? '/test/'
-    : '/'
-})
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//     transpileDependencies:true,
+// 	publicPath: process.env.NODE_ENV === 'production'
+//     ? '/test/'
+//     : '/'
+// })
 
 module.exports = {
+	configureWebpack: {
+    performance: {
+    maxEntrypointSize: 10000000,
+    maxAssetSize: 10000000
+    }
+	},
 	publicPath: process.env.NODE_ENV === 'production'
     ? '/test/'
     : '/'
+		
 }

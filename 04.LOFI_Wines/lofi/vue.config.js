cee74b671ee1583368ac9182576e53e4f8a2ef6b
Min Kyung <01aldrn10@gmail.com>
2023-12-07 21:22:10 +0900

    s

--- 04.LOFI_Wines/lofi/vue.config.js
+++ 04.LOFI_Wines/lofi/vue.config.js
@@ -6,12 +6,12 @@ module.exports = {
     maxAssetSize: 10000000
     }
 	},
-	// publicPath: '/lofi'
-	transpileDependencies:true
+	publicPath: '/lofi'
+	// transpileDependencies:true
 
 
     // publicPath: process.env.NODE_ENV === 'production'
-    //   ? '/lofi/'
+    //   ? '/lofi'
     //   : '/'
 
 

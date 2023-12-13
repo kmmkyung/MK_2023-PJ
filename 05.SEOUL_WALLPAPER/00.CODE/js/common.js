a750995612398d96075e9a6ce71de2784f740589
Min Kyung <01aldrn10@gmail.com>
2023-12-13 21:23:27 +0900

    s

--- 05.SEOUL_WALLPAPER/00.CODE/js/common.js
+++ 05.SEOUL_WALLPAPER/00.CODE/js/common.js
@@ -1,9 +1,56 @@
-// import gnbData from '../assets/data/data-gnbData';
 import svgData from '../assets/data/data-svgData.js';
+import gnbData from '../assets/data/data-gnbData.js';
 
 window.addEventListener('DOMContentLoaded',function(){
   // [ header ]
   //// logo
   const logo = document.querySelector('.pc-header .logo');
   logo.innerHTML = svgData.logoSvg_b;
+
+  //// gnb
+  const gnbAll = document.querySelector('.gnb-all');
+  let gnbDataCode = '';  
+
+  gnbDataCode += `<ul class="gnb">`
+  for(let title in gnbData){
+    gnbDataCode += /* html */`
+      <li class="gnb-menu">${title}
+        <div class="gnb-menu__area">
+        <ol>
+          <li class="gnb-menu__title">
+            <span class="title-ko">${title}</span>
+            <img src="./assets/svg/gnb-dropdown-icon.svg" alt="dropdown-icon">
+            <div class="title-content">
+              <img src="./assets/svg/talk-icon.svg" alt="talk-icon">
+    `
+    for(let enTitle in gnbData[title]){
+      gnbDataCode += /* html */`
+              <span class="title-en">${enTitle}</span>
+              </div>
+          </li>
+        `
+      for(let subTitle in gnbData[title][enTitle]){
+      gnbDataCode += /* html */`
+          <li class="gnb-menu__item">${subTitle}
+      `
+        for(let menu of gnbData[title][enTitle][subTitle]){
+        gnbDataCode += /* html */`
+            <span>${menu}</span>
+      `
+        }
+      gnbDataCode += /*html*/`
+          </li>
+        `;
+      }
+    gnbDataCode += /*html*/`
+    `;
+    }
+    gnbDataCode += /*html*/`
+        </ol>
+      </div>
+    </li>
+    `;
+  }
+  gnbDataCode +=  /*html*/`</ul>`
+  gnbAll.innerHTML = gnbDataCode;
 })

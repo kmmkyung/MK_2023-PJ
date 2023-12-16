9991d0c38c9b1fb0a3447a8c8c3995f2fb1ff4ed
Min Kyung <01aldrn10@gmail.com>
2023-12-16 19:07:38 +0900

    s

---
+++ 05.SEOUL_WALLPAPER/00.CODE/js/index.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded',function(){
+  // section1
+  //// banner
+  const swiper = new Swiper('.section-1__swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+
+  });
+})

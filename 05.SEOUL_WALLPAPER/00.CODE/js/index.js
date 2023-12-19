3f0a9ac3fc27842422e3c0fe839a2cd3637d1e59
Min Kyung <01aldrn10@gmail.com>
2023-12-20 02:30:44 +0900

    s

--- 05.SEOUL_WALLPAPER/00.CODE/js/index.js
+++ 05.SEOUL_WALLPAPER/00.CODE/js/index.js
@@ -13,6 +13,28 @@ window.addEventListener('DOMContentLoaded',function(){
       el: '.swiper-pagination',
       clickable: true
     },
-
   });
+
+  // section2
+  //// card
+  const section2 = document.querySelector('#section-2');
+
+  window.addEventListener('scroll',function(){
+    let section2WinTop = section2.getBoundingClientRect().top
+    let section2PageSize = section2.offsetHeight
+    // console.log(window.scrollY-section2PageSize);
+    // console.log(section2PageSize/2*1);
+    
+    // if(section2WinTop<=0){
+    //   section2.style.position = 'fixed';
+    // }
+    // if(window.scrollY - section2PageSize >= section2PageSize/2*1){
+    //   section2.style.position = 'relative';
+    // }
+  })
+
+
+
+
+
 })
